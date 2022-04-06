8f2e95acb2ad3125c32cf817306737e9d3eeeb5b
joissa <itsjoshissa@gmail.com>
2022-04-06 11:51:02 -0400

    Plots done
    The plots are now replicated.

--- star_info.py
+++ star_info.py
@@ -47,6 +47,8 @@ def plot_star(data, info = None, savepath=""):
 
         plt.figtext(0.58,0.65, star_info, fontsize=12)
 
+    r_R = data['r/R'] #so we don't need to call it a bunch
+
     ## FIRST PLOT - density, mass, luminosity, temperature
 
     pl_rho = data['rho(r)']/info.rhoc
@@ -54,9 +56,6 @@ def plot_star(data, info = None, savepath=""):
     pl_T = data['T(r)']/info.Tc
     pl_L = data['L(r)']/info.L
 
-    r_R = data['r/R']
-
-    #need to relativize the coordinates of the text
     axes[0,0].plot(r_R, pl_rho, color = s.colours[0])
     axes[0,0].text(0.03, pl_rho[max(r_R[r_R < 0.1].index.values)], r'$\rho$', color = s.colours[0], fontsize=14)
     axes[0,0].plot(r_R, pl_M, color = s.colours[1], ls='dashed')
@@ -72,13 +71,12 @@ def plot_star(data, info = None, savepath=""):
 
     Pc = data['P'][0]
 
-    #need to relativize the coordinates of the text
     axes[1,0].plot(r_R, data['P']/Pc, color = s.colours[0])
-    axes[1,0].text(0.4, 0.1, r'$P_{tot}$', color = s.colours[0], fontsize=14)
+    axes[1,0].text(r_R[max(data['P'][data['P']/Pc>0.1].index.values)], 0.12, r'$P_{tot}$', color = s.colours[0], fontsize=14)
     axes[1,0].plot(r_R, data['Pgas']/Pc, color = s.colours[1], ls='dashed')
-    axes[1,0].text(0.15, 0.45, r'$P_{gas}$', color = s.colours[1], fontsize=14)
+    axes[1,0].text(1.1*r_R[max(data['Pgas'][data['Pgas']/Pc>0.45].index.values)], 0.5, r'$P_{gas}$', color = s.colours[1], fontsize=14)
     axes[1,0].plot(r_R, data['Pdeg']/Pc, color = s.colours[2], ls=(0, (5, 10)))
-    axes[1,0].text(0.05, 0.1, r'$P_{deg}$', color = s.colours[2], fontsize=14)
+    axes[1,0].text(0.05, 1.3*data['Pdeg'][max(r_R[r_R < 0.05].index.values)]/Pc, r'$P_{deg}$', color = s.colours[2], fontsize=14)
     axes[1,0].plot(r_R, data['Ppho']/Pc, color = s.colours[3], ls='dotted')
     axes[1,0].text(0.2, 0.05, r'$P_{\gamma}$', color = s.colours[3], fontsize=14)
 
@@ -95,11 +93,10 @@ def plot_star(data, info = None, savepath=""):
 
     ## FOURTH PLOT - log(kappa)
 
-    #need to relativize the coordinates of the text
     axes[1,1].plot(r_R, np.log10(data['kappa']), color = s.colours[0])
-    axes[1,1].text(0.5, 1, r'$\kappa$', color = s.colours[0], fontsize=14)
+    axes[1,1].text(0.5, 1.5*np.log10(data['kappa'][max(r_R[r_R < 0.5].index.values)]), r'$\kappa$', color = s.colours[0], fontsize=14)
     axes[1,1].plot(r_R, np.log10(data['kappaff']), color = s.colours[1], ls='dashed')
-    axes[1,1].text(0.8, 2, r'$\kappa_{ff}$', color = s.colours[1], fontsize=14)
+    axes[1,1].text(0.8, 1.7*np.log10(data['kappa'][max(r_R[r_R < 0.8].index.values)]), r'$\kappa_{ff}$', color = s.colours[1], fontsize=14)
     axes[1,1].plot(r_R, np.log10(data['kappaHm']), color = s.colours[2], ls=(0, (5, 10)))
     axes[1,1].text(0.85, 9, r'$\kappa_{H^-}$', color = s.colours[2], fontsize=14)
     axes[1,1].plot(r_R, np.log10(data['kappaes']), color = s.colours[3], ls='dotted')
@@ -108,16 +105,19 @@ def plot_star(data, info = None, savepath=""):
     axes[1,1].set_ylabel(r'log$_{10}$($\kappa$) (cm$^2$/g)')
     axes[1,1].set_ylim(-2, 10)
 
-    #need to relativize the coordinates of the text
+    ## FIFTH PLOT - dL/dr
+
+    lum_max = round(2.1*np.nanmax(data['dL/dr'])*info.R/info.L,)
+
     axes[2,1].plot(r_R, data['dL/dr']*info.R/info.L, color = s.colours[0])
-    axes[2,1].text(0.6, 10, r'$dL$/$dr$', color = s.colours[0], fontsize=14)
+    axes[2,1].text(0.5, lum_max*0.8, r'$dL$/$dr$', color = s.colours[0], fontsize=14)
     axes[2,1].plot(r_R, data['dLpp/dr']*info.R/info.L, color = s.colours[2], ls=(0, (5, 10)))
-    axes[2,1].text(0.6, 9, r'$dL_{pp}$/$dr$', color = s.colours[2], fontsize=14)
+    axes[2,1].text(0.5, lum_max*0.7, r'$dL_{pp}$/$dr$', color = s.colours[2], fontsize=14)
     axes[2,1].plot(r_R, data['dLcno/dr']*info.R/info.L, color = s.colours[3], ls='dotted')
-    axes[2,1].text(0.6, 8, r'$dL_{CNO}$/$dr$', color = s.colours[3], fontsize=14)
+    axes[2,1].text(0.5, lum_max*0.6, r'$dL_{CNO}$/$dr$', color = s.colours[3], fontsize=14)
 
     axes[2,1].set_ylabel(r'd$L_*$/d$r$ (L$_*$/R$_*$)')
-    axes[2,1].set_ylim(0, round(2.1*np.nanmax(data['dL/dr'])*info.R/info.L,))
+    axes[2,1].set_ylim(0, lum_max)
     axes[2,1].set_xlabel(r'$r$/$R_*$')
 
     #settings aesthetics for all plots
