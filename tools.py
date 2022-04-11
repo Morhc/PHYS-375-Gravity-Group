cd73cb79f49dc70e63f071175bc68c2f09db63d4
Darshak Patel <102433527+TheShak5@users.noreply.github.com>
2022-04-11 18:57:38 -0400

    Brought back the RK45 adaptive step code I wrote previously and the opacity proxy function (del_tau).

--- tools.py
+++ tools.py
@@ -38,6 +38,40 @@ def dydr(r,y) :
 
     return dydr
 
+def RKF45Method_adaptive(f,r,y,h):
+    '''Second RK4Method which invovled adpative step sizes
+       Source: https://en.wikipedia.org/wiki/Runge%E2%80%93Kutta%E2%80%93Fehlberg_method#Implementing_an_RK4(5)_Algorithm
+       The coefficietns for 'A' were taken from the table title "COEFFICIENTS FOR RK4(5), FORMULA 2 Table III in Fehlberg"
+
+       Input(s):
+       - 'r' - array of radius
+       - 'y' - 5 column matrix representing [rho,Temp,Mass,Luminosity,OpticalDepth]
+       - 'h' - the stepsize
+
+       Output(s):
+       - 'y5' - 5th order solutions to ODE
+       - 'hnew' - the new adpted stepsize
+    '''
+    K1 = h*f(r,y)
+    K2 = h*f( r + (1/4)*h, y + (1/4)*K1 )
+    K3 = h*f( r + (3/8)*h, y + (3/32)*K1 + (9/32)*K2 )
+    K4 = h*f( r + (12/13)*h, y + (1932/2197)*K1 + (-7200/2197)*K2 + (7296/2197)*K3 )
+    K5 = h*f( r + (1)*h, y + (439/216)*K1 + (-8)*K2 + (3680/513)*K3 + (-845/4104)*K4 )
+    K6 = h*f( r + (1/2)*h, y + (-8/27)*K1 + (2)*K2 + (-3544/2565)*K3 + (1859/4104)*K4 + (-11/40)*K5 )
+
+    # The truncation error (TE)
+    TE = abs( (1/360)*K1 + (0)*K2 + (-128/4275)*K3 + (-2197/75240)*K4 + (1/50)*K5 + (2/55)*K6 )
+
+    # the error estimate of the RKF45 method
+    y5 = y + (25/216)*K1 + (1408/2565)*K3 + (2197/4101)*K4 - (1/5)*K5
+    y4 = y + (16/135)*K1 + (6656/12825)*K3 + (28561/56430)*K4 - (9/50)*K5 + (2/55)*K6
+    error = abs( y5 - y4)
+
+    # Calcuating the new step-sizes
+    hnew = min((0.9)*h*( (error/TE)**(1/5) ))
+
+    return(y5,hnew)
+
 def epsilon(rho, T):
     """Calculates epsilon using Equations 8 and 9 from the project description.
     INPUTS:
@@ -354,6 +388,9 @@ def luminosity_check(R, L, T):
 
     return f
 
+def del_tau(rho,T,r,M,L):
+    '''This fucntion will calcualte the opacity proxy'''
+    return (kappa(rho,T)*(rho**2) / abs(drho_dr(rho, T, r, M,L)))
 
 # ---------------------------------
 # Modification of Gravity Section:
